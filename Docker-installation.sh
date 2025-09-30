38416dc6a3c12d71ef0e82c1c3b0988a98730476
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-30 21:41:39 +0100

    add

--- Docker-installation.sh
+++ Docker-installation.sh
@@ -3,4 +3,6 @@ sudo apt install apt-transport-https ca-certificates curl \
 gnupg-agent software-properties-common
 
 # Download the Docker gpg key and add it to the apt package manager:
-
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-\
+keyring.gpg
