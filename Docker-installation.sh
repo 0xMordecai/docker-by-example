f7056b22557b7ed5ddf7a73cb9032df7a6279516
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-30 21:41:10 +0100

    add

--- Docker-installation.sh
+++ Docker-installation.sh
@@ -1,3 +1,6 @@
 # Install the supporting tools that Docker would need to run:
 sudo apt install apt-transport-https ca-certificates curl \
 gnupg-agent software-properties-common
+
+# Download the Docker gpg key and add it to the apt package manager:
+
