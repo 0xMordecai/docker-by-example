9cc8689087c06dacee10122d6b63ec69869d681e
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-10-07 22:59:23 +0100

    Add

--- Docker-monitoring/node_exporter-shell.sh
+++ Docker-monitoring/node_exporter-shell.sh
@@ -1,2 +1,9 @@
 # Create a dedicated user
 sudo useradd --no-create-home --shell /bin/false node_exporter
+
+# Download the latest Node Exporter release
+# Go to the /tmp directory:
+cd /tmp
+
+# Then download the latest version:
+curl -LO https://github.com/prometheus/node_exporter/releases/latest/download/node_exporter-1.9.1-linux-amd64.tar.gz
