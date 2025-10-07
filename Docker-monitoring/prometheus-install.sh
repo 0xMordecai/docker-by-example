62d4c0980e0c429122279d2fff80f2becd82a938
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-10-07 22:21:13 +0100

    Add

--- Docker-monitoring/prometheus-install.sh
+++ Docker-monitoring/prometheus-install.sh
@@ -1,3 +1,14 @@
 # Download the latest Prometheus binary
 cd /tmp
 wget https://github.com/prometheus/prometheus/releases/latest/download/prometheus-3.5.0-linux-amd64.tar.gz
+
+# Unpack it:
+tar xvf prometheus-3.5.0-linux-amd64.tar.gz
+cd prometheus-3.5.0-linux-amd64
+
+# Move binaries to system path
+sudo mv prometheus promtool /usr/local/bin/
+
+# Verify installation:
+prometheus --version
+
