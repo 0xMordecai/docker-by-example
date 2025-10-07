7d4a477ec105fdcf98ccba981138b1d8cc688878
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-10-07 23:13:04 +0100

    Add

--- Docker-monitoring/node_exporter-shell.sh
+++ Docker-monitoring/node_exporter-shell.sh
@@ -6,7 +6,8 @@ sudo useradd --no-create-home --shell /bin/false node_exporter
 cd /tmp
 
 # Then download the latest version:
-wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.darwin-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+
 # Unpack it:
 tar xvf node_exporter-1.9.1-linux-amd64.tar.gz
 cd node_exporter-1.9.1-linux-amd64
