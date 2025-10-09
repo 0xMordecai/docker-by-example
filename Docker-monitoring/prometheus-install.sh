b48f22a3078039830e85196cddc798d698946183
Mohamed Lahrach <sir.lahrach.mohamed@gmail.com>
2025-10-09 18:19:20 +0100

    Update prometheus-install.sh

--- Docker-monitoring/prometheus-install.sh
+++ Docker-monitoring/prometheus-install.sh
@@ -36,8 +36,7 @@ Type=simple
 ExecStart=/usr/local/bin/prometheus \
     --config.file=/etc/prometheus/prometheus.yml \
     --storage.tsdb.path=/var/lib/prometheus/ \
-    --web.console.templates=/etc/prometheus/consoles \
-    --web.console.libraries=/etc/prometheus/console_libraries
+    --web.console.templates=/etc/prometheus/conso
 
 [Install]
 WantedBy=multi-user.target
